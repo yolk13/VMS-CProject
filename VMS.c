6953a14a82d4c7309dfd2145601e2f60850f9d30
Prakash Gyawali <48545068+yolk13@users.noreply.github.com>
2023-02-24 03:17:41 +0000

    new file:   .vscode/settings.json 	new file:   VMS.c

---
+++ VMS.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+// Declaration of Users
+struct admin{
+    char user;
+    char pass;
+}au;
+struct Eofficer{
+    char user;
+    char pass;
+    char election;
+    int status;
+}eo[10];
+struct voter{
+    int id;
+    int status;
+    int DOB;
+    char area;
+}v[];
+//Declaration of Candidate
+struct candidate{
+    int id;
+    char symbol;
+    char rparty;
+    char name;
+    char postof;
+    char area;
+    int DOB;
+}c[700];
+
