6569c3f972ab82b6206a9f0892402b62b6297840
Prakash Gyawali <yolk13@outlook.com>
2023-03-13 18:11:17 +0545

    fixed login issue. working with admin options.

--- VMS.c
+++ VMS.c
@@ -1,53 +1,86 @@
 #include<stdio.h>
 #include<string.h>
+#include<conio.h>
 // Declaration of Users
-struct admin{
-    char user;
-    char pass;
-}au;
+struct Madmin{
+    char user[8];
+    char pass[8];
+	char DOB[6];
+}au = {"admin","admin","010101"};
 struct Eofficer{
     char user;
     char pass;
     char election;
+    char DOB;
     int status;
 }eo[10];
-struct voter{
+struct Voter{
     int id;
     int status;
-    int DOB;
+    char DOB;
     char area;
 }v[5000];
 //Declaration of Candidate
-struct candidate{
+struct Candidate{
     int id;
     char symbol;
     char rparty;
     char name;
     char postof;
     char area;
-    int DOB;
-}c[700];
+    char DOB;
+    int vote;
+}can[700];
 
 //main Code
 
 int main(){
-login();
+//	strcpy(au.user,"admin");
+//	strcpy(au.pass,"admin");
+//	au.DOB=010101;
+		
+	login();
 }
 
-int login(){
-printf("====================================\n");
-printf("WELCOME TO VOTING MANAGEMENT SYSYEM (VMS)");
+int login()
+{
+	struct Madmin madmin;
+	struct Eofficer eofficer;
+	struct Voter voter;
+	struct Candidate candidate;
+	printf("=========================================\n \t|WELCOME TO VOTING MANAGEMENT SYSTEM (VMS)");
+	printf("\n\n \t|PLEASE LOGIN BEFORE YOU CAN CONTINUE\n");
+	printf(" \n\nEnter your ID---:");
+	char uid[5];
+	scanf("%s",&uid);
+	printf(" Enter your DateOfBirth---:");
+	char udb[6];
+	scanf("%s",&udb);
+	int x;
+	if((strcmp(au.user,uid)==0)&&(strcmp(au.DOB,udb)==0))
+	{
+		admin();
+	}
+	else
+	{
+		Eofficer();
+	}
 }
 
 int admin(){
-	
+	system("cls");
+	printf("=========================================\n \t|WELCOME TO VMS ADMIN PORTAL");
+	printf("\n\n");
+	printf("1. option a \n2. option b \n");
 }
 
 int Eofficer(){
-	
+	system("cls");
+	printf("WELCOME TO VMS ELECTION OFFICER PORTAL");
 }
 
 int voter(){
+	system("cls");
 	
 }
 
